d87c5b80783874ea3dd0c39b0027cc030d45b2cf
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-12-10 18:57:35 -0300

    ref: changed scaling on window

--- src/Editor.java
+++ src/Editor.java
@@ -2,7 +2,7 @@ import models.Janela;
 
 public class Editor {
     public static void main(String[] args) {
-        System.setProperty("sun.java2d.uiScale","1.5");
+        System.setProperty("sun.java2d.uiScale","2");
         new Janela();
     }
 }
